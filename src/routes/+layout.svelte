2a69e21449b43d685ab63e9957c983e4949b78d3
Jkuffler <jakuffler@gmail.com>
2023-06-19 16:06:44 -0400

    nav and layout set

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<nav>
+    <ul>
+        <li><a href='/'>home</a></li>
+        <li><a href='/about'>about</a></li>
+        <li><a href='/experience'>experience</a></li>
+        <!-- <li><a href='/resume.pdf'>resume</a></li> -->
+        <li><a href='/about'>projects</a></li>
+    </ul>
+</nav>
+<slot></slot>
+
+<style>
+    ul {
+        display: flex;
+        list-style-type: none;
+        gap: 2em;
+    }
+</style>
