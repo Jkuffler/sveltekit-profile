48c17cfadf01a752a0ed0189f2a386da622168e7
Jkuffler <jakuffler@gmail.com>
2023-07-11 19:09:58 -0400

    experimental animations on home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<script></script>
-<h1> Jason Kuffler</h1>
-<style></style>
+<script>
+	import { elasticOut } from 'svelte/easing';
+	import { fade, fly, slide } from 'svelte/transition';
+
+	let showing = false;
+</script>
+
+<label>
+	<input bind:checked={showing} type="checkbox" />
+</label>
+
+{#if showing}
+	<div>
+		<h1 transition:fade>Jason Kuffler</h1>
+	</div>
+{/if}
+
+<style>
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	h1 {
+		margin-top: 9em;
+		font-family: 'Courier New', Courier, monospace;
+	}
+</style>
