2cda734edc42cf7ddcbfef50d6d1ebda7bcdc405
Jkuffler <jakuffler@gmail.com>
2023-07-26 10:04:02 -0400

    setting template for animation and styling from root path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,28 @@
 <script>
-
 </script>
 
 <nav>
-    <ul>
-        <li><a href='/'>home</a></li>
-        <li><a href='/about'>about</a></li>
-        <li><a href='/experience'>experience</a></li>
-        <li><a href='https://docs.google.com/document/d/1RvBvzJs4o3lVxu07kFWXLkd1WR5ryvmFKj_LSQCdoSM/edit?usp=sharing' target="_blank">resume</a></li>
-        <li><a href='/projects'>projects</a></li>
-    </ul>
+	<ul>
+		<li><a href="/">home</a></li>
+		<li><a href="/about">about</a></li>
+		<li><a href="/experience">experience</a></li>
+		<li>
+			<a
+				href="https://docs.google.com/document/d/1RvBvzJs4o3lVxu07kFWXLkd1WR5ryvmFKj_LSQCdoSM/edit?usp=sharing"
+				target="_blank">resume</a
+			>
+		</li>
+		<li><a href="/projects">projects</a></li>
+	</ul>
 </nav>
-<slot></slot>
+<slot />
 
 <style>
-    ul {
-        display: flex;
-        list-style-type: none;
-        gap: 2em;
-    }
+	nav,
+	ul {
+		display: flex;
+		list-style-type: none;
+		gap: 2em;
+		font-family: 'Courier New', Courier, monospace;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,46 @@
 <script>
-	import { slide } from 'svelte/transition';
-	import { onMount } from 'svelte';
-	let showing = false;
-	onMount(() => {
-		showing=true;
-	})
-	
+  import { slide } from 'svelte/transition';
+  import { onMount } from 'svelte';
+  let showing = false;
+  let loopCount = 0;
+  const totalLoops = 10;
+
+  const loopAnimate = () => {
+    showing = true;
+    loopCount++;
+
+    if (loopCount <= totalLoops) {
+      // Increase the font size with each pass
+      const fontSize = 24 + loopCount * 4;
+      document.documentElement.style.setProperty('--font-size', `${fontSize}px`);
+
+      // Wait for the animation to complete before re-triggering it
+      setTimeout(() => {
+        loopAnimate();
+      }, 500);
+    }
+  };
+
+  onMount(() => {
+    loopAnimate();
+  });
 </script>
 
 {#if showing}
-	<div>
-		<h1 transition:slide>Jason Kuffler</h1>
-	</div>
+<div>
+  <h1 in:slide out:slide style="font-size: var(--font-size, 24px)">Jason Kuffler</h1>
+</div>
 {/if}
 
 <style>
-	div {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	h1 {
-		margin-top: 9em;
-		font-family: 'Courier New', Courier, monospace;
-	}
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    margin-top: 5em;
+    font-family: 'Courier New', Courier, monospace;
+  }
 </style>
